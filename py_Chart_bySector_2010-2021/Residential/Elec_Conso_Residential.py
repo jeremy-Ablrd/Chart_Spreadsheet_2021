a8a887ff755780157b5d6c1871afdfdcdb511d00
jerem_Ablrd <jeremy97426@gmail.com>
2022-11-03 15:57:47 +0400

    Add new code folder about fuel and elect conso.

--- py_Chart_bySector_2010-2021/Residential/Elec_Conso_Residential.py
+++ py_Chart_bySector_2010-2021/Residential/Elec_Conso_Residential.py
@@ -3,14 +3,18 @@ import pandas as pd
 import seaborn as sns
 
 """
-Unit : 
- - GWh
+Legend :
+ - Kerosene
+ - Gasoil
+ - LPG
+
+Unit : TOE
 """
 
 path = "C:/Users/jerem/Desktop/Chart_Spreadsheet_2021/Spreadsheet for the preparation of Energy Reports ver-9_Update_returned_4Oct22.xlsx"
 file = pd.read_excel(path, sheet_name="Growth 2010-2021", header=69, index_col=1)
 
-list_line = [31]
+list_line = [0]
 
 df_final = pd.DataFrame([])
 for line in list_line:
@@ -19,26 +23,25 @@ for line in list_line:
     df = df.round(0)
     df_final = pd.concat([df_final, df], axis=1)
 
-df_final.columns = ['GWh']
 print(df_final)
 
 sns.set(style='white')
 colors = ['#4C72B0']
-plt.figure(figsize=(9, 6))
-ax = sns.barplot(x=df_final.index, y=df_final['GWh'], palette=colors)
+plt.figure(figsize=(12, 9))
+ax = sns.barplot(x=df_final.index, y=df_final['Residential'], palette=colors)
 
 label_color = ['black' for i in range(0, len(list_line))]
 # label_color.append('#903B3F')
 #
 for i, container in enumerate(ax.containers[:]):
-    ax.bar_label(container, color=label_color[i], label_type='edge', padding=0.9)
+    ax.bar_label(container, color=label_color[i], label_type='center', padding=0.9)
 
-plt.title('Total Electricity Consumption in the Service Sector', fontsize=16)
+plt.title('Electricity Consumption in the Residential Sector', fontsize=16)
 plt.grid(visible=True, axis='y')
 
 plt.xlabel('Year')
 plt.xticks(rotation=0)
-plt.ylim(0, 320)
+#plt.ylim(0, 320)
 plt.ylabel('GWh')
 
 # handles, labels = plt.gca().get_legend_handles_labels()
@@ -46,6 +49,6 @@ plt.ylabel('GWh')
 # print(order)
 # plt.legend([handles[i] for i in order], [labels[i] for i in order])
 
-path_savefig = "C:/Users/jerem/Desktop/Chart_Spreadsheet_2021/Figure_Sector/Service"
-plt.savefig(f'{path_savefig}/SimplyBarchart_spread_TotalElectConso_Service.png', transparent=True, dpi=300)
+path_savefig = "C:/Users/jerem/Desktop/Chart_Spreadsheet_2021/Figure_Sector/Residential"
+plt.savefig(f'{path_savefig}/SimplyBarchart_spread_ElectConso_Residential.png', transparent=True, dpi=300)
 plt.show()
--- py_Chart_bySector_2010-2021/Service/Fuel_conso_Service.py
+++ py_Chart_bySector_2010-2021/Service/Fuel_conso_Service.py
@@ -3,14 +3,18 @@ import pandas as pd
 import seaborn as sns
 
 """
-Unit : 
- - GWh
+Legend :
+ - Kerosene
+ - Gasoil
+ - LPG
+
+Unit : TOE
 """
 
 path = "C:/Users/jerem/Desktop/Chart_Spreadsheet_2021/Spreadsheet for the preparation of Energy Reports ver-9_Update_returned_4Oct22.xlsx"
-file = pd.read_excel(path, sheet_name="Growth 2010-2021", header=69, index_col=1)
+file = pd.read_excel(path, sheet_name="Growth 2010-2021", header=188, index_col=1)
 
-list_line = [31]
+list_line = [16, 17, 19]
 
 df_final = pd.DataFrame([])
 for line in list_line:
@@ -19,33 +23,31 @@ for line in list_line:
     df = df.round(0)
     df_final = pd.concat([df_final, df], axis=1)
 
-df_final.columns = ['GWh']
 print(df_final)
 
 sns.set(style='white')
-colors = ['#4C72B0']
-plt.figure(figsize=(9, 6))
-ax = sns.barplot(x=df_final.index, y=df_final['GWh'], palette=colors)
+colors = ['#A25B75', '#C44E52', '#903B3F']
+ax = df_final.plot(kind='bar', stacked=True, color=colors, figsize=(12, 9))
 
 label_color = ['black' for i in range(0, len(list_line))]
 # label_color.append('#903B3F')
 #
 for i, container in enumerate(ax.containers[:]):
-    ax.bar_label(container, color=label_color[i], label_type='edge', padding=0.9)
+    ax.bar_label(container, color=label_color[i], label_type='center', padding=0.9)
 
-plt.title('Total Electricity Consumption in the Service Sector', fontsize=16)
+plt.title('Fuel Consumption in the Service Sector', fontsize=16)
 plt.grid(visible=True, axis='y')
 
 plt.xlabel('Year')
 plt.xticks(rotation=0)
-plt.ylim(0, 320)
-plt.ylabel('GWh')
+#plt.ylim(0, 320)
+plt.ylabel('TOE')
 
-# handles, labels = plt.gca().get_legend_handles_labels()
-# order = [i for i in reversed(range(0, len(list_line)))]
-# print(order)
-# plt.legend([handles[i] for i in order], [labels[i] for i in order])
+handles, labels = plt.gca().get_legend_handles_labels()
+order = [i for i in reversed(range(0, len(list_line)))]
+print(order)
+plt.legend([handles[i] for i in order], [labels[i] for i in order])
 
 path_savefig = "C:/Users/jerem/Desktop/Chart_Spreadsheet_2021/Figure_Sector/Service"
-plt.savefig(f'{path_savefig}/SimplyBarchart_spread_TotalElectConso_Service.png', transparent=True, dpi=300)
+plt.savefig(f'{path_savefig}/BarchartDetailled_spread_FuelConso_Service.png', transparent=True, dpi=300)
 plt.show()
